b6934b87eb07da1283b242b257f884a01f716ab1
Jenny Shaw <bloo.j.shaw@gmail.com>
2019-05-06 20:32:44 +0000

    Automatically backed up by Learn

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -2,8 +2,7 @@ def my_collect(collection)
   i = 0
   new_collection = []
   while i < collection.length 
-    yield collection[i]
-
+    yield (collection[i])
     i += 1
   end
   new_collection
@@ -12,7 +11,7 @@ end
 languages = ['ruby', 'javascript', 'python', 'objective-c']
 
 my_collect(languages) do |language| 
-  language.upcase
+  new_collection << language.upcase
 end
 
 students = ['Tim Jones', 'Tom Smith', 'Sophie Johnson', 'Antoin Miller']
